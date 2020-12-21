1e52ce9a18b4f1c4f5631abeccc3b666f4a9d50d
LottieFile <106222151+LottieFile@users.noreply.github.com>
2020-12-21 16:59:05 +0000

    initial import.

--- app/src/androidTest/java/com/sajidkhanafridi/coin/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/sajidkhanafridi/coin/ExampleInstrumentedTest.java
@@ -12,3 +12,16 @@ import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.sajidkhanafridi.coin", appContext.getPackageName());
+    }
+}
