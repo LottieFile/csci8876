d217a11dac2037352aec94d97521ff4079da1713
LottieFile <106222151+LottieFile@users.noreply.github.com>
2021-01-24 18:23:58 +0000

    fixed some problems

---
+++ app/src/main/java/com/sajidkhanafridi/coin/MainActivity.java
@@ -0,0 +1,20 @@
+
+package com.sajidkhanafridi.coin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final int[] coinSet ={R.drawable.front, R.drawable.back};
+        final ImageView tossImage = findViewById(R.id.toss_Image);
+        Button tossButton = findViewById(R.id.toss_Button);
