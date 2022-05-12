7b5d488fc035232e29397fe51eabe3e34aab8bdf
LottieFile <106222151+LottieFile@users.noreply.github.com>
2022-05-12 10:39:25 +0000

    build: Upgrade phan-taint-check-plugin from 1.5.x to 2.0.1

---
+++ app/src/test/java/com/sajidkhanafridi/coin/ExampleUnitTest.java
@@ -0,0 +1,11 @@
+
+package com.sajidkhanafridi.coin;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
