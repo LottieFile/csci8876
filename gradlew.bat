c09b03a1cf670ca89f7d4e29fb81551849a092ab
LottieFile <106222151+LottieFile@users.noreply.github.com>
2023-04-26 07:32:33 +0000

    prepare release

--- gradlew.bat
+++ gradlew.bat
@@ -41,4 +41,6 @@ echo.
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
 echo.
 echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java inst
+echo location of your Java installation.
+
+goto 
