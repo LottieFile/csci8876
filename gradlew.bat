d5d3ad95065428bcb6722ffed056940ec9163409
LottieFile <106222151+LottieFile@users.noreply.github.com>
2023-05-31 20:02:03 +0000

    modify css

--- gradlew.bat
+++ gradlew.bat
@@ -76,4 +76,7 @@ if "%ERRORLEVEL%"=="0" goto mainEnd
 rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
 rem the _cmd.exe /c_ return code!
 if  not "" == "%GRADLE_EXIT_CONSOLE%" exit 1
-exit /b 
+exit /b 1
+
+:mainEnd
+if "%
